ef3fc2ad63c15c5a4f5b71ec8c4ced0f09dfbff6
esthyo <joyben68@gmail.com>
2024-02-06 15:08:13 +0100

    finished
    finished

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menuLinks");
+  const icon = document.querySelector(".hamburger_icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
